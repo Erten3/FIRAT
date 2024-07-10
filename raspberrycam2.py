b24979e652903a5122aa5b2227971b2565971b25
Erten3 <154601830+Erten3@users.noreply.github.com>
2024-07-10 11:23:00 +0100

    Update raspberrycam2.py

--- raspberrycam2.py
+++ raspberrycam2.py
@@ -8,36 +8,43 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 8000))
 
 data = b""
-payload_size = struct.calcsize("L")
+payload_size = struct.calcsize("Q")
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
     while len(data) < payload_size:
-        packet = client_socket.recv(4096)
+        packet = client_socket.recv(4*1024)
         if not packet: break
         data += packet
 
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
 
     while len(data) < msg_size:
-        data += client_socket.recv(4096)
+        packet = client_socket.recv(4*1024)
+        if not packet: break
+        data += packet
 
     frame_data = data[:msg_size]
     data = data[msg_size:]
 
     frame = pickle.loads(frame_data)
+
+    # Renk dönüşümü yerine direkt olarak GRAY formatında alalım
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
+    # İmajı göstermek için renk formatını yeniden ayarlayabiliriz
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Veya BGR2GRAY olarak kalabilir, bağlı olarak
     cv2.imshow('Face Tracking', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 client_socket.close()
 cv2.destroyAllWindows()
+
